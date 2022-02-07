72d6ba3e01fc06afe86eaaf78c7f14dacb6c2fe5
Jimmy <1064425721@qq.com>
2022-02-07 17:48:57 +0800

    修改了样式，修改了距离日期规则，修正了判断周末问题，增加显示当前时间

--- 2，Timing the festival/Timing.js
+++ 2，Timing the festival/Timing.js
@@ -1,9 +1,33 @@
 //today
 let ToDay = new Date()
-let toMonth = ToDay.getMonth() + 1
-let toDay = ToDay.getDate()
-let thisDay = `${toMonth}月${toDay}日`
-document.getElementById("today").innerHTML = thisDay;
+let toYear = ToDay.getFullYear()
+
+function getDay() {
+    let toMonth = ToDay.getMonth() + 1
+    let toDay = ToDay.getDate()
+    let thisDay = `${toMonth}月${toDay}日`
+    document.getElementById("thisDay").innerHTML = thisDay;
+}
+
+// 时间
+function getTime() {
+    let Time = new Date()
+    let toHour = Time.getHours()
+    let toMinute = Time.getMinutes()
+    let toSecond = Time.getSeconds()
+    if (toSecond < 10) {
+        toSecond = "0" + toSecond
+    }
+    let thisTime = `${toHour}:${toMinute}:${toSecond}`
+    console.log(toHour, toMinute, toSecond, thisTime)
+        // setTimeout(getTime, 1000)
+    document.getElementById("thisTime").innerHTML = thisTime;
+}
+window.onload = function() {
+    getDay();
+    getTime()
+}
+window.setInterval(getTime, 1000);
 
 // 设置节日-计算-赋值
 
@@ -32,7 +56,8 @@ switch (weekday) {
         week = 6;
         break;
 }
-if (5 <= week < 7) {
+if (4 < weekday && weekday < 7) {
+    console.log("weekday1", weekday)
     theWeek = `你离下周周末还有${week}天`
     console.log(theWeek)
     document.getElementById("weeks").innerHTML = theWeek;
@@ -43,30 +68,26 @@ if (5 <= week < 7) {
 }
 
 //圣诞节
-let ChristmasDay = new Date(2021, 12, 25)
+let ChristmasDay = new Date(toYear, 12, 25)
 ChristmasDay.setMonth(ChristmasDay.getMonth() - 1)
-let Christmas = Math.ceil((ChristmasDay - ToDay) / (1000 * 60 * 60 * 24));//向上取整
+let Christmas = Math.ceil((ChristmasDay - ToDay) / (1000 * 60 * 60 * 24)); //向上取整
 document.getElementById("Christmas").innerHTML = Christmas;
 
 //过年
-let YearDay = new Date(2022, 02, 01,)
+let YearDay = new Date(toYear + 1, 01, 21, )
 YearDay.setMonth(YearDay.getMonth() - 1)
-let Years = Math.ceil((YearDay - ToDay) / (1000 * 60 * 60 * 24));//向上取整
+let Years = Math.ceil((YearDay - ToDay) / (1000 * 60 * 60 * 24)); //向上取整
 document.getElementById("years").innerHTML = Years;
 
 //元旦
-let NewYear = new Date(2022, 01, 01,)
+let NewYear = new Date(toYear + 1, 01, 01, )
 NewYear.setMonth(NewYear.getMonth() - 1)
-let NewYears = Math.ceil((NewYear - ToDay) / (1000 * 60 * 60 * 24));//向上取整
+let NewYears = Math.ceil((NewYear - ToDay) / (1000 * 60 * 60 * 24)); //向上取整
 document.getElementById("Newyears").innerHTML = NewYears;
 
 //元宵
-let LanternFestival = new Date(2022, 02, 15,)
+let LanternFestival = new Date(toYear + 1, 02, 15, )
 LanternFestival.setMonth(LanternFestival.getMonth() - 1)
 let Lanterns = Math.ceil((LanternFestival - ToDay) / (1000 * 60 * 60 * 24));
 document.getElementById("Lanterns").innerHTML = Lanterns;
 console.log(Years, Christmas, ToDay.getDay(), Lanterns)
-
-
-
-
